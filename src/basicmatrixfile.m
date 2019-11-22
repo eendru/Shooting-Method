bb62e2270ebdec3f77b09c80efeb52bcd8d07a8a
eendru <p.andrey.m@yandex.com>
2019-11-22 11:54:36 +0300

    Update basicmatrixfile.m

--- src/basicmatrixfile.m
+++ src/basicmatrixfile.m
@@ -1,8 +1,8 @@
-  function [ result ] = basicmatrixfile(dimension) 
-  result = zeros(dimension);
-result(1, 1) = 2.000000; 
+function [ result ] = basicmatrixfile(dimension) 
+result = zeros(dimension);
+result(1, 1) = 2.000000; 
 result(1, 2) = 1.000000; 
 result(2, 1) = 2.000000; 
 result(2, 2) = -1.000000; 
 end 
-
+
